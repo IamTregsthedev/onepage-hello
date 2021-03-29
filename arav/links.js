8984abe564a3efafd96a063737fe8de3c94eb90d
Arav Narula <proteximportsexports@gmail.com>
2021-03-28 23:22:14 -0400

    Create link working

---
+++ arav/links.js
@@ -0,0 +1,54 @@
+// This is where you will edit your links.
+
+
+
+// Basic Information
+const name = "Arav"   // Enter your First Name
+const lastname = "Narula" // Last Name
+const email = "mailto:arav.1tb@gmail.com"
+
+
+// Social Links
+
+const twitter = "https://tregsthedev.link/twitter"
+const social2 = ""
+const social2 = ""
+const social2 = ""
+const social2 = ""
+const social2 = ""
+const social2 = ""
+const social2 = ""
+const social2 = ""
+const social2 = ""
+
+
+
+
+
+
+// Custom Links
+
+const custom1 = "https://www.yourlink1here.com"
+const custom2  "https://www.yourlink1here.com"
+const custom3 = "https://www.yourlink1here.com"
+
+// Icons
+
+const iconforname1 = ""
+      const iconforname2 = ""
+        const iconforname3 = ""
+          const iconforname4 = ""
+            const iconforname5 = ""
+              
+
+
+
+
+// Common Font Aweasoem Icons
+/*
+fa-twitter
+fa-slack
+fa-telegram
+fa-email
+fa-inbox
+fa-pintrest
